ddb1359024a619c25c466e1ec4b9fbe16ab9ca32
mariuscozma11 <mariuscozmabe@gmail.com>
2026-02-04 00:49:00 +0200

    Cors

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/mariuscozma11/practice-go-api/auth"
 	"github.com/mariuscozma11/practice-go-api/db"
@@ -19,11 +21,20 @@ func main() {
 	if err != nil {
 		log.Fatal("Error creating DB Pool:", err)
 	}
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://mariuscozma.co", "http://localhost:5173"}
+	router.Use(cors.New(config))
 	// Unprotected routes:
 	router.GET("/posts/:id", posts.GetPostByID)
 	router.POST("/login", auth.Login)
 	router.GET("/posts", posts.GetPosts)
 	router.GET("/refresh", auth.RefreshToken)
+	router.GET("/", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"message": "Success",
+		})
+	})
 
 	// Protected routes:
 	router.Use(auth.ProtectedRoute())
